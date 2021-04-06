537d5b5536ff7706254082cbf2cb07a0a362dc0a
Yoo-Jin Hwang <yoojin.hwang00@gmail.com>
2021-04-06 10:04:27 -0700

    Fixed error and it now runs fine, issue with fps tho

--- ImageProcessing/AWS_sync.py
+++ ImageProcessing/AWS_sync.py
@@ -20,7 +20,7 @@ def record_video(dir):
 	cmd_list = []
 	output_path = os.path.expanduser(dir + f'{stringNow}.h264')
 	print(output_path)
-	cmd1 = 'raspivid -t -15000 -o ' + output_path
+	cmd1 = 'raspivid -t 15000 -fps 60 -o ' + output_path
 	cmd2 = 'MP4Box -add ' + output_path + ' ' + dir + f'{stringNow}.mp4'
 	#cmd2 = f'MP4Box -add {stringNow}.h264 {stringNow}.mp4'
 	cmd3 = 'rm ' + dir + f'{stringNow}.h264'
@@ -32,13 +32,13 @@ def record_video(dir):
 command_list = record_video(os.path.expanduser('~/Desktop/ClinicPictures/'))
 
 # Take the picture 
-#for i in range(len(command_list)):
-#	print("index",i)
-#	print("command list at i", command_list[i])
-#	subprocess.call(command_list[i].split())
-#	time.sleep(10)
+for i in range(len(command_list)):
+	print("index",i)
+	print("command list at i", command_list[i])
+	subprocess.call(command_list[i].split())
+	time.sleep(10)
 
-subprocess.call(command_list[0].split())
+#subprocess.call(command_list[0].split())
 
 # Sync directory to AWS bin 
 # sync_cmd = f'aws s3 sync ~/Desktop/ClinicPictures s3://hmc-clinic-2021'

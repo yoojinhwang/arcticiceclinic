00a8e7965915ef2128ece836521193f28c5c405e
Ben Jin <bwjin@g.hmc.edu>
2021-05-04 15:46:19 -0400

    updated Particle Model code to include an input that supresses graphs and fixed falilures output.  StatsExCode was renamed to Regressions

--- MATLAB_Simulation/Batch_Data_Collection/ParticleModelBen.m
+++ MATLAB_Simulation/Batch_Data_Collection/ParticleModelBen.m
@@ -1,4 +1,4 @@
-function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChange)
+function OutData = ParticleModelBen(numParticles,TimeDep,Twind,stdevWindChange,graph)
     %numParticles: Number of iterations/particles
     %TimeDep: Triggers time dependence (1 on, 0 off)
     %modDenom: Only relevant in TD models, frequency of wind change in
@@ -41,14 +41,14 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
     rBlower = 0.762; %meters, 60in/2
     vFan = 12.94; %m/s, when cfm = 50000 and d = 60in
     vblower = 10; %Fblower/mp; %the initial velocity of the particle, given vFan
-    %vwind = 10; %meters
-    %Re = vwind*Dp*rhof/1.68e-5;
 
     %Other
-    Cd = 0.659692; %2.6; FURTHER WORK: Vary CD with reynolds number (pretty well behaved eqn for spheres)
+    Cd = 0.659692; %FURTHER WORK: Vary CD with reynolds number
     g = 9.81;
-    failedCount=0;
 
+    %Adjustment to ode solver to prevent squiggles, increases comp time
+    options = odeset('RelTol',1e-5,'AbsTol',1e-7); 
+    
     %Create Output Vectors
     xFin = zeros(numParticles,1);
     yFin = zeros(numParticles,1);
@@ -61,15 +61,13 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
     AvgWindY = zeros(numParticles,1);
     AvgTheta = zeros(numParticles,1);
     AvgMag = zeros(numParticles,1);
-    %Timing for debug
     times = zeros(numParticles,1);
-
+    failed= zeros(numParticles,1);
 
     for i = 1:numParticles
         %%Create Random Parameters
         runitime=tic;
         %Random Wind
-        %vblower = random('Normal', 20,5);
         vwind = random('Normal', Vwind_mean,Vwind_standarddev);
         thetaWind = random('Normal',thetaWind_mean,thetaWind_standarddev);
         wind(1) = vwind*cos(thetaWind); %vwindX
@@ -78,10 +76,10 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
 
 
         %Random Diameter
-        %Dp = random('Normal',Dp_mean,Dp_standarddev); %Use for standard gaussian distribution (you'll have to make some changes to the if statements if you still want to incorporate min/max cutoffs)
         DpOK=0;
         while DpOK ~=1    
             Dp =random('Gamma', k, th);
+            %Dp = random('Normal',Dp_mean,Dp_standarddev); %Use for standard gaussian distribution
             if Dp<Dp_max && Dp>Dp_min
                 DpOK=1;
             end
@@ -97,6 +95,8 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
         mp = rhop*Vp;
 
         FBlower = A*rhof*vFan^2;
+        
+%%%%%%%%%%%%%%%%DOM, THIS IS WHERE THERE WOULD BE A SWITCH WHERE WE CHOOSE BETWEEN ANALYTICAL AND ODE SOLUTIONS%%%%%%%%%%%%%%%
 
         %Pack parameters into vector for createState Function
         parameters = zeros(6,1);        %Adjust if you want to add more parameters
@@ -107,7 +107,7 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
         parameters(5) = g;
         parameters(6) = rBlower;
         parameters(7) = FBlower;
-        parameters(8) = modDenom;
+        parameters(8) = Twind;
         parameters(9) = stdevWindChange;
 
         %Create Vector of Initial Values
@@ -121,17 +121,16 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
         
         %Set how many seconds the ODE's are solved to.  Increase if particles aren't hitting the ground
         %Based on testing, critical ODE time limit is almost entirely determined by particle diameter, with smaller particles requiring more time
+        %This may change when the mass distribution is corrected
         if Dp>50e-6
-            ODETimeLim = 200; %should bo 100, 200
+            ODETimeLim = 120; %120 and 200 seconds is a good start for K1 HGMs
         else
-            ODETimeLim = 300;
+            ODETimeLim = 200;
         end
-        %Adjustment to ode solver to prevent squiggles, increases comp time
-        options = odeset('RelTol',1e-5,'AbsTol',1e-7); 
 
-        %Solving for v of particle v' = .....
+        %Call Ode23 to solve governing equations
         windChangeCounter = 0; %Ensures wind changes just once per time interval
-        [t,y] = ode23(@(t,x)createState(t,x,parameters,TimeDep),[0,ODETimeLim],x0,options);
+        [t,y] = ode45(@(t,x)createState(t,x,parameters,TimeDep),[0,ODETimeLim],x0,options);
         xPosition = y(:,1);
         yPosition = y(:,2);
         zPosition = y(:,3);
@@ -141,83 +140,77 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
 
         %Find the index for when particles reach ground.
         [minZ,groundIndex] = min(abs(zPosition));
-        %Check to see if they actually hit the ground or just ran out of
-        %time
-        %FURTHER WORK: Find average z velocity of particles in order to
-        %determine how many meters of error are possible for a given
-        %failure height (eg, asuume vx = 10 m/s, find time (t) it takes
-        %for a particle to fall from minZ to the ground, t*vx = final
-        %position error).  Use said limit to impelemet a acceptable error
-        %threshold instead of the arbritrary choice I made
-        if minZ>0.1 %This is an estimation, not an actual count
-            fprintf('Error: Particle may not have hit ground, Zpos = %.2f \n',minZ)
-            failedCount = failedCount+1;
+        %Check to see if they actually hit the ground or just ran out of time
+
+        if minZ>0.5 && t(groundIndex)==ODETimeLim %This is an estimation, not an actual count
+            fprintf('Error: Particle may not have hit ground, Zpos = %.2f, t = %f\n',minZ,t(groundIndex))
+            failed(i) = 1;
+        elseif minZ>0.5 %Adjust this value to modify final height tolerance
+            fprintf('Warning: Final particle height outside of tolerance\n')
         end
         
-        %{
-        %^^Put a { here to pause state plotting
+        if graph
+            figure(1)
+            subplot(2,1,1)
+            plot(t(1:groundIndex), zVelocity(1:groundIndex));
+            hold on
+            title('Z Velocity and Position')
+            xlabel('time (s)')
+            ylabel('Z Velocity (m/s)')
+            subplot(2,1,2)
+            plot(t(1:groundIndex), zPosition(1:groundIndex));
+            hold on
+            xlabel('time (s)')
+            ylabel('Z Position (m/s)')
+            %
+            figure(2)
+            subplot(2,1,1)
+            plot(t(1:groundIndex), xVelocity(1:groundIndex));
+            hold on
+            title('X Velocity and Position')
+            xlabel('time (s)')
+            ylabel('X Velocity (m/s)')
+            subplot(2,1,2)
+            plot(t(1:groundIndex), xPosition(1:groundIndex));
+            hold on
+            xlabel('time (s)')
+            ylabel('X Position (m/s)')
 
-        figure(1)
-        subplot(2,1,1)
-        plot(t(1:groundIndex), zVelocity(1:groundIndex));
-        hold on
-        title('z velocity + position')
-        xlabel('time')
-        ylabel('z-direction velocity')
-        subplot(2,1,2)
-        plot(t(1:groundIndex), zPosition(1:groundIndex));
-        hold on
-        xlabel('time')
-        ylabel('z-direction position')
+            figure(3)
+            subplot(2,1,1)
+            plot(t(1:groundIndex), yVelocity(1:groundIndex));
+            hold on
+            title('Y Velocity and Position')
+            xlabel('time (s)')
+            ylabel('Y Velocity (m/s)')
+            subplot(2,1,2)
+            plot(t(1:groundIndex), yPosition(1:groundIndex));
+            hold on
+            xlabel('time (s)')
+            ylabel('Y Position (m/s)')
 
-        figure(2)
-        subplot(2,1,1)
-        plot(t(1:groundIndex), xVelocity(1:groundIndex));
-        hold on
-        title('x velocity + position')
-        xlabel('time')
-        ylabel('x-direction velocity')
-        subplot(2,1,2)
-        plot(t(1:groundIndex), xPosition(1:groundIndex));
-        hold on
-        xlabel('time')
-        ylabel('x-direction position')
+            figure(4)
+            plot(xPosition(1:groundIndex), yPosition(1:groundIndex))
+            hold on
+            xlabel('X Position')
+            ylabel('Y Position')
+            title('Particle Pathlines')
 
-        figure(3)
-        subplot(2,1,1)
-        plot(t(1:groundIndex), yVelocity(1:groundIndex));
-        hold on
-        title('y velocity + position')
-        xlabel('time')
-        ylabel('y-direction velocity')
-        subplot(2,1,2)
-        plot(t(1:groundIndex), yPosition(1:groundIndex));
-        hold on
-        xlabel('time')
-        ylabel('y-direction position')
-
-        figure(4)
-        plot(xPosition(1:groundIndex), yPosition(1:groundIndex))
-        hold on
-        xlabel('x position')
-        ylabel('y position')
-        title('y vs x')
-
-        figure(6)
-        plot3(xPosition(1:groundIndex),yPosition(1:groundIndex),zPosition(1:groundIndex),'LineWidth',3) 
-        title('trajectory paths')
-        xlabel('x distance')
-        ylabel('y distance')
-        zlabel('z distance')
-        hold on
-        %}
+            figure(6)
+            plot3(xPosition(1:groundIndex),yPosition(1:groundIndex),zPosition(1:groundIndex),'LineWidth',3) 
+            title('Particle Pathlines')
+            xlabel('X Position')
+            ylabel('Y Position')
+            zlabel('Z Position')
+            grid on
+            hold on
+        end
+        
         
         %Output Data Collection
         xFin(i) = xPosition(groundIndex);
         yFin(i) = yPosition(groundIndex);
-        zFin(i) = zPosition(groundIndex); %Useful for debuging, should be all zeros.  If there are nonzero values, increase the ammount of time the ODE solver runs for (this will slow the program down).
-        %Note, longer time periods also seem to increase ODE solve step
-        %intervals, which introduces its own inaccuracies so be careful
+        zFin(i) = zPosition(groundIndex);
         tFin(i) = t(groundIndex);
         Ds(i) = Dp;
         rhos(i) = rhop;
@@ -231,57 +224,57 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
             Mags = sqrt(WindSpeedTracker(:,2).^2+WindSpeedTracker(:,1).^2);
             AvgMag(i) = mean(Mags);
         end
-        %Print data about most recent run to command window, can eliminate for speed
+        %Print data about most recent run to command window, can comment for speed or clarity
         fprintf('particle %u, elapsed time %.2f, xFin = %.2f, yFin = %.2f, flight time = %.2f \n', i,times(i),xFin(i),yFin(i),tFin(i))
     end
     
     %Format Output Data to Table
-    failedCounts = zeros(numParticles,1);
-    failedCounts(1) = failedCount; %variables need to be same length, just look at first index for data
     if TimeDep == 0
-        OutData = table(Ds,rhos,mps,AvgWindX,AvgWindY,tFin,failedCounts,times,xFin,yFin,zFin);
+        OutData = table(Ds,rhos,mps,AvgWindX,AvgWindY,tFin,failed,times,xFin,yFin,zFin);
     else
-        OutData = table(Ds,rhos,mps,AvgWindX,AvgWindY,AvgTheta,AvgMag,tFin,failedCounts,times,xFin,yFin,zFin);
+        OutData = table(Ds,rhos,mps,AvgWindX,AvgWindY,AvgTheta,AvgMag,tFin,failed,times,xFin,yFin,zFin);
     end
     
+    fprintf('%d Particles modeled with %d failures \n',numParticles,sum(failed))
+    
     %Final Scatter Plot
-    figure(5)
-    scatter(xFin,yFin)
-    xlabel('X Position (m)')
-    ylabel('Y Position (m)')
-    title('Scatterplot of Final HGM Positions')
-    fprintf('%d Particles modeled with %d failures \n',numParticles,failedCount)
-    %Turn off all the holds on the plots.
-    %
-    figure(1)
-    subplot(2,1,1)
-    hold off
-    subplot(2,1,2)
-    hold off
+    if graph
+        figure(5)
+        scatter(xFin,yFin)
+        xlabel('X Position (m)')
+        ylabel('Y Position (m)')
+        title('Scatterplot of Final HGM Positions')
 
-    figure(2)
-    subplot(2,1,1)
-    hold off
-    subplot(2,1,2)
-    hold off
 
-    figure(3)
-    subplot(2,1,1)
-    hold off
-    subplot(2,1,2)
-    hold off
+        %Turn off all the holds on the plots.
+        figure(1)
+        subplot(2,1,1)
+        hold off
+        subplot(2,1,2)
+        hold off
 
-    figure(4)
-    hold off
+        figure(2)
+        subplot(2,1,1)
+        hold off
+        subplot(2,1,2)
+        hold off
 
-    figure(6)
-    hold off
+        figure(3)
+        subplot(2,1,1)
+        hold off
+        subplot(2,1,2)
+        hold off
+
+        figure(4)
+        hold off
+
+        figure(6)
+        hold off
+    end
     %}
     
     toc(startTime) %Elapsed time from start of script
-    %patch([xPosition(1:groundIndex) nan],[yPosition(1:groundIndex) nan],[zPosition(1:groundIndex) nan],[zPosition(1:groundIndex) nan],'EdgeColor','interp','FaceColor','none')
-
-    %function dx = createState(t,x,param)
+    
     function dx = createState(t,x,param,TimeDep)
         %Creates the state vector containing info:
         %[Vx,Vy,Vz,ax,ay,az] from the input x = [xpos,ypos,zpos,Vx,Vy,Vz]
@@ -291,26 +284,23 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
         mp = param(4);
         g = param(5);
         rBlower = param(6);
-        modDenom = param(8);
+        Twind = param(8);
         stdevWindChange = param(9);
-        %While Particles are within blower radius, have a contant upward force.
         xPos = x(1);
         yPos = x(2);
+        %While Particles are within blower radius, have a contant upward force.
         if xPos^2 + yPos^2 <= rBlower^2
             Fblower = param(7);
         else
             Fblower = 0;
         end
 
-        %%%%%%%%%% THIS IS WHERE TIME DEPENDEND WIND IS CALLED %%%%%%%%%%%%%%%
-        %Limits the number of times the function changes the wind speed,
-        %otherwise you just get random noise.  Implemented by calling timeWind
-        %every 'modDenom' seconds
+        %Call Time Dependent Wind
         if TimeDep
             floorTime=floor(t);
-            if ~mod(floorTime,modDenom) && ~ismember(floorTime,windChangeCounter) %mod(floorTime, modDenom) = 0 whever floor time is evenly divisible by modDenom
-                wind = timeWind(wind,modDenom,stdevWindChange);
-                windChangeCounter = [windChangeCounter,floorTime]; %Adds the current time to the windChangeCounter to ensure that the next time (which will probably be floored to the same integer) does not call timeWind again
+            if ~mod(floorTime,Twind) && ~ismember(floorTime,windChangeCounter) %mod(floorTime, Twind) = 0 when floor time is evenly divisible by Twind
+                wind = timeWind(wind,Twind,stdevWindChange);
+                windChangeCounter = [windChangeCounter,floorTime]; %Adds the current time to the windChangeCounter to ensure that the next time step (which will probably be floored to the same integer) does not call timeWind again
                 WindSpeedTracker = [WindSpeedTracker;wind];
             end
         end
@@ -345,11 +335,9 @@ function OutData = ParticleModelBen(numParticles,TimeDep,modDenom,stdevWindChang
     end
 
 
-    function  wind = timeWind(wind,modDenom,stdevWindChange)
+    function  wind = timeWind(wind,Twind,stdevWindChange)
         %time dependent wind based on a random walk
-        %Change parameters to adjust random walk
-        wind(1) = wind(1) + modDenom*random('Normal', 0,stdevWindChange);
-        wind(2) = wind(2) + modDenom*random('Normal', 0,stdevWindChange);
-        %}
+        wind(1) = wind(1) + Twind*random('Normal', 0,stdevWindChange);
+        wind(2) = wind(2) + Twind*random('Normal', 0,stdevWindChange);
     end
 end
